2eeeb05f962bff09b41d91a7d807d6348a68337d
Luis Bonsembiante <luis.bonsembiante@overproofdev.com>
2024-03-18 11:40:18 -0300

    compile vs implementation

--- RNImageRotate/ImageRotateModule.m
+++ RNImageRotate/ImageRotateModule.m
@@ -32,32 +32,32 @@ RCT_EXPORT_METHOD(rotateImage:(NSURLRequest *)imageURL
                   errorCallback:(RCTResponseErrorBlock)errorCallback)
 {
 
-  [_bridge.imageLoader loadImageWithURLRequest:imageURL callback:^(NSError *error, UIImage *image) {
+  [[_bridge moduleForName:@"ImageLoader" lazilyLoadIfNecessary:YES] loadImageWithURLRequest:imageURL callback:^(NSError *error, UIImage *image) {
     if (error) {
       errorCallback(error);
       return;
     }
-      
+
     // calculate the size of the rotated view's containing box for our drawing space
     UIView *rotatedViewBox = [[UIView alloc] initWithFrame:CGRectMake(0, 0, image.size.width, image.size.height)];
     CGAffineTransform t = CGAffineTransformMakeRotation(DegreesToRadians([angle doubleValue]));
     rotatedViewBox.transform = t;
     CGSize rotatedSize = rotatedViewBox.frame.size;
-      
+
     // Create the bitmap context
     UIGraphicsBeginImageContext(rotatedSize);
     CGContextRef bitmap = UIGraphicsGetCurrentContext();
-      
+
     // Move the origin to the middle of the image so we will rotate and scale around the center.
     CGContextTranslateCTM(bitmap, rotatedSize.width / 2, rotatedSize.height / 2);
-      
+
     // Rotate the image context
     CGContextRotateCTM(bitmap, DegreesToRadians([angle doubleValue]));
-      
+
     // Now, draw the rotated/scaled image into the context
     CGContextScaleCTM(bitmap, 1.0, -1.0);
     CGContextDrawImage(bitmap, CGRectMake(-image.size.width / 2, -image.size.height / 2, image.size.width, image.size.height), [image CGImage]);
-      
+
     UIImage *rotatedImage = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
 
